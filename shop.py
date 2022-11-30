4ee66041d10495c43fd22615c902d25fe70bef62
Przemyslaw Bil <g00398317@gmit.ie>
2022-11-30 13:31:25 +0000

    Function finalize_transaction added

--- shop.py
+++ shop.py
@@ -7,7 +7,7 @@
 import csv
 import os
 from ShopFunctions import display_menu, get_user_selection, get_user_number, defineMenuChoices
-from ShopErrors import BudgetTooLowError
+from ShopErrors import BudgetTooLowError, NotEnoughStockError
 
 def create_and_stock_shop(path):
 
@@ -51,8 +51,7 @@ def read_customer(csv_path):
             "name" : first_row[0],
             "cash": float(first_row[1]),
             "payment_due": 0,
-            "shopping_list": [],
-            "shopping_basket": []
+            "shopping_list": []
         }
 
         for row in csv_reader:
@@ -60,6 +59,9 @@ def read_customer(csv_path):
             product={
                 "name" : row[0],
                 "qty" : int(row[1]),
+                "price": 0,
+                "basket_qty": 0,
+                "bag_qty": 0
             }
 
             customer["shopping_list"].append(product)
@@ -82,30 +84,31 @@ def print_shop(s):
 def print_customer(cust):
 
     print("\n{} wants to buy: ".format(cust["name"]))
-    #print("Name: {}, Cash: {}".format(cust["name"], cust["cash"]))
-
-    # 'i' will be used as the shopping list counter
-    i = 0
 
     print("Shopping list:")
-    for product in cust["shopping_basket"]:
+    for product in cust["shopping_list"]:
 
-        #print(cust["shopping_list"][0]['qty'])
-        req_qty = cust["shopping_list"][i]['qty']
-
-        print("NAME: {},  PRICE: €{:.2f}, REQUIRED QUANTITY: {:3d}, IN THE BASKET: {:3d}".format(product["name"], product["price"], req_qty, product["qty"]))
-
-        # increase the shopping basket counter
-        i+=1
+        print("NAME: {},  PRICE: €{:.2f}, REQUIRED QUANTITY: {:3d}, IN THE BASKET: {:3d}".format(product["name"], product["price"], product["qty"], product["basket_qty"]))
 
     rest = cust["cash"] - customer["payment_due"]
     print("------------------------------------------")
-    print("The total cost would be: €{:.2f}, he would have €{:.2f} left".format(customer["payment_due"], rest))   
+    print("The total cost would be: €{:.2f}, he would have €{:.2f} left".format(customer["payment_due"], rest))
+
+# method to save all exceptions to csv file
+def addToExceptionsFiles(ef_path, msg):
+
+    # as per https://www.pythontutorial.net/python-basics/python-write-csv-file/ and 
+    # https://stackoverflow.com/questions/13203868/how-to-write-to-csv-and-not-overwrite-past-text 
+    with open(ef_path, 'a', encoding='UTF8', newline='') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        # as per https://stackoverflow.com/questions/1816880/why-does-csvwriter-writerow-put-a-comma-after-each-character
+        writer.writerow([msg])   
 
 
 # function to look for items from the customre shopping list in the shops stock,
 # putting found items to the shopping basket and calculating the total cost
-def fill_shopping_basket(customer, shop):
+def fill_shopping_basket(customer, shop, ef_path):
 
     # keep tally of the total cost of all the products in the shoppibg list
     payment_due = 0
@@ -119,36 +122,83 @@ def fill_shopping_basket(customer, shop):
             if list_item["name"] == stock_item["name"]:
                 
                 # make sure shop has enough stock to fulfill the order
-                if stock_item["qty"]>=list_item["qty"]:
-                    qty = list_item["qty"]
+                if stock_item["qty"] >= list_item["qty"]:
+                    qty = list_item["qty"]                    
                 else:
                 # if it doesn't, put in the basket whatever is left in stock
                     qty = stock_item["qty"]
-
-                payment_due += qty*stock_item["price"]
-
-                # Create a basket item
-                basket_item= {
-                "name" : list_item["name"],
-                "qty" : qty,
-                "price": stock_item["price"]
-                }
+                    err_msg = "There is not enough {} in stock. Actual stock: {}, required Stock {} ".format(list_item["name"] , qty, list_item["qty"])
+                    addToExceptionsFiles(ef_path, err_msg)
+                
+                # Remove required qty from the shops stock
+                stock_item["qty"]-=qty
 
                 # put the required amount of selected product into the shopping basket
-                customer["shopping_basket"].append(basket_item)
+                list_item["basket_qty"] = qty                
+
+                list_item["price"] = stock_item["price"]
+
+                # calculate full amount due for all the available items
+                payment_due += qty*stock_item["price"]
     
     # assign the payment_due in customer dict with sum of the values of items in the shopping basket
     customer["payment_due"] = payment_due
 
-    return customer
+    return customer, shop
+
+
+
+def finilize_transaction(customer, shop, ef_path):
+
+    # try to pay for every basket item individually
+    for basket_item in customer["shopping_list"]:
+
+        while basket_item['basket_qty']>0:
+
+            payment_due =  basket_item['basket_qty']*basket_item['price']
+            # Check if the customer has enough money to pay for the item in the basket
+            if customer['cash'] >= payment_due:
+
+                # Pay for the items
+                customer['cash'] -= payment_due
+                shop['cash'] += payment_due
+                # Empty the basket into the bag
+                basket_item['bag_qty'] = basket_item['basket_qty']
+                basket_item['basket_qty'] = 0
+                
+                # stop the while loop
+                # break
+            # If the customer doesn't have enough money to pay for all the items, remove one item from the basket                
+            else:
+                # Add error message to the Exceptions file
+                err_msg = "{} has not enough money to pay for {} units of {} worth {:.2f} as he/she has only {:.2f}".format(
+                                customer['name'], 
+                                basket_item['basket_qty'], 
+                                basket_item['name'], 
+                                basket_item['basket_qty']*basket_item['price'], 
+                                customer['cash']  
+                            )
+                addToExceptionsFiles(ef_path, err_msg)                                
+                # remove one item from the basket and check again if the customer can afford it
+                basket_item['basket_qty']-=1
+                # find the product in the shop and return one item from the basket back to the shop
+                for product in shop['products']:
+                    if product['name'] == basket_item['name']:
+                        product['qty'] += 1
+                        break
+    
+    # return changed customer and shop variables
+    return customer, shop
 
 # main for function call
 if __name__ == "__main__":
 
     # File path for the shop's csv file
     shop_csv_path = 'stock.csv'
-    # File path for suctomer's csv file
-    customer_csv_path = 'customer.csv'    
+    # File path for customer's csv file
+    customer_csv_path = 'customer.csv'
+    # File path for the Exceptions csv file
+    exceptions_csv_path ='Exceptions.csv'
 
     # Initialize myShop variable by reading shop status from the csv file
     myShop = create_and_stock_shop(shop_csv_path)
@@ -174,7 +224,7 @@ if __name__ == "__main__":
             customer = read_customer(customer_csv_path)
 
             # fill customers basket based on the shopping list and available stock in shop
-            customer = fill_shopping_basket(customer, myShop)
+            customer, myShop = fill_shopping_basket(customer, myShop, exceptions_csv_path)
 
             print("\nShop and the Customer pre-transaction:\n")
             print_shop(myShop)
@@ -182,6 +232,8 @@ if __name__ == "__main__":
 
             # Pause to give user chance to read Customer and Shop states before the transaction
             input("Press ENTER to finilize the sale")
+
+            finilize_transaction(customer, myShop, exceptions_csv_path)
 
             print("\nShop and the Customer post-transaction:\n")
 
